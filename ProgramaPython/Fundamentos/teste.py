81c305e65b0b8935e97b9118bc44c74f21416709
Jorge Alberto de Paula <jorge.adp.s2@gmail.com>
2025-10-03 15:32:45 -0300

    Fundamentos linguagem python

---
+++ ProgramaPython/Fundamentos/teste.py
@@ -0,0 +1,12 @@
+
+# Leitura da palavra
+palavra = input()
+
+# Leitura do número inteiro
+n = int(input())
+
+# Impressão da palavra duas vezes concatenada
+print(palavra + palavra)
+
+# Impressão da palavra repetida n vezes
+print(palavra * n)
